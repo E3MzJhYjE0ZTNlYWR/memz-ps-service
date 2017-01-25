a6e34cabe131bf5d408ec9889b463f9857fb6765
Andrey Chausenko <info@memz.co>
2017-01-25 17:06:23 +1100

    Releasing version 1.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# extract sources
 cd /home
 openssl enc -d -aes-256-cbc -in package.zip.enc -out package.zip -pass pass:$UNLOCK_KEY
 unzip package.zip
 rm -rf package.*
 mkdir -p /go/src/github.com/Beh01der/memz-ps
 mv ./package/* /go/src/github.com/Beh01der/memz-ps
+
+# build
 cd /go/src/github.com/Beh01der/memz-ps
 go install
-cd /home
 
+# cleanup
+rm -rf /go/pkg/*
+rm -rf /go/src/*
+
+# start service
+cd /home
 /go/bin/memz-ps $@
